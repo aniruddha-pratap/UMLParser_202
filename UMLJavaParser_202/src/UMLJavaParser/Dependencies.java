37786894e03da91e974df3127dbf507247cd02b8
aniruddha-pratap <aniruddhapratap.singh@sjsu.edu>
2017-03-11 23:04:13 -0800

    New code in dependencies

--- UMLJavaParser_202/src/UMLJavaParser/Dependencies.java
+++ UMLJavaParser_202/src/UMLJavaParser/Dependencies.java
@@ -6,4 +6,20 @@ public class Dependencies {
 
 	private String interfce="";
 	private String classes="";
+	
+	public String getinterfce() {
+		return interfce;
+	}
+	
+	public void setinterfce(String interfce) {
+		this.interfce = interfce;
+	}
+	
+	public String getClassName() {
+		return classes;
+	}
+	
+	public void setClassName(String className) {
+		this.classes = className;
+	}
 }
