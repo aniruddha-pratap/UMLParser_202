f59615c3470e4e126a2392cba4c9f5802e7f80a4
aniruddha-pratap <aniruddhapratap.singh@sjsu.edu>
2017-03-18 13:42:37 -0700

    organized parser core

--- UMLJavaParser_202/src/UMLJavaParser/UMLJavaParser.java
+++ UMLJavaParser_202/src/UMLJavaParser/UMLJavaParser.java
@@ -1,14 +1,31 @@
 package UMLJavaParser;
 import java.util.*;
-//import java.io.*;
+
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.body.BodyDeclaration;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.ConstructorDeclaration;
+import com.github.javaparser.ast.body.FieldDeclaration;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.body.ModifierSet;
+import com.github.javaparser.ast.body.Parameter;
+import com.github.javaparser.ast.body.TypeDeclaration;
+import com.github.javaparser.ast.body.VariableDeclarator;
+import com.github.javaparser.ast.body.VariableDeclaratorId;
+import com.github.javaparser.ast.type.ClassOrInterfaceType;
+import com.github.javaparser.ast.type.PrimitiveType;
+import com.github.javaparser.ast.type.ReferenceType;
+import com.github.javaparser.ast.type.VoidType;
 
 public class UMLJavaParser {
 	
-	String inputFile = null;
-	ArrayList<String> interfaces = new ArrayList<String>();
-	ArrayList<String> variables = new ArrayList<String>();
-	ArrayList<String> methods = new ArrayList<String>();
-	ArrayList<String> parameters = new ArrayList<String>();
+	private String inputFile = null;
+	private ArrayList<String> implementedInterfaces = new ArrayList<String>();
+	private ArrayList<String> classVariables = new ArrayList<String>();
+	private ArrayList<String> classMethods = new ArrayList<String>();
+	private ArrayList<String>  classMethodParams= new ArrayList<String>();
 	
 	
 }
