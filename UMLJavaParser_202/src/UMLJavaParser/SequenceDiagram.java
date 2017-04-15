fb3a2765012c1af77864f4b88614a90fdd489214
aniruddha-pratap <aniruddhapratap.singh@sjsu.edu>
2017-04-15 13:02:47 -0700

    Sequence Diagram new parser code

--- UMLJavaParser_202/src/UMLJavaParser/SequenceDiagram.java
+++ UMLJavaParser_202/src/UMLJavaParser/SequenceDiagram.java
@@ -15,15 +15,17 @@ public class SequenceDiagram {
 	private StringBuilder umlCode;
 	private FileInputStream inputStream;
 	private ArrayList<CompilationUnit> compilationUnitArray;
-	
+		
 	public SequenceDiagram(String in, String out){
 		path = new String[2];
 		path[0] = in;
 		path[1] = out;
 	}
 	
-	public void sequenceDiagram() throws Exception{
+	public void sequenceDiagram(String className, String functionName) throws Exception{
 		try{
+			Map<String, String> classMap = new HashMap<String, String>();
+		    Map<String, List<MethodCallExpr>> methodClMap  = new HashMap<String, List<MethodCallExpr>>();
 			compilationUnitArray = new ArrayList<CompilationUnit>();
 			File files = new File(path[0]);
 			for(File file: files.listFiles()){
@@ -44,9 +46,10 @@ public class SequenceDiagram {
 					while(bodyIterator.hasNext()){
 						BodyDeclaration bodyDeclaration = (BodyDeclaration) bodyIterator.next();
 						if(bodyDeclaration instanceof MethodDeclaration){
+							MethodDeclaration methodDeclaration = (MethodDeclaration) bodyIterator.next();
 							List<MethodCallExpr> methodCallExpr = new ArrayList<MethodCallExpr>();
-							List<Node> methodDeclaration = bodyDeclaration.getChildrenNodes();
-							Iterator<Node> methodDecIterator = methodDeclaration.listIterator(); 
+							List<Node> methodDecList = methodDeclaration.getChildrenNodes();
+							Iterator<Node> methodDecIterator = methodDecList.listIterator(); 
 							while(methodDecIterator.hasNext()){
 								Object blockStmt = methodDecIterator.next();
 								if(blockStmt instanceof BlockStmt){
@@ -62,11 +65,18 @@ public class SequenceDiagram {
 										}
 									}
 								}
+								methodClMap.put(methodDeclaration.getName(), methodCallExpr);
+								classMap.put(methodDeclaration.getName(), cName.toString());
 							}
 						}
 					}
 				}
 			}
+			umlCode.append("@startuml\n actor user #black\n ");
+			String temp = "user" + " -> " + className + " : " + functionName + "\n";
+			umlCode.append(temp);
+			temp= "activate " + classMap.get(functionName) + "\n";
+			umlCode.append(temp);
 		}catch(Exception e){
 			
 		}finally{
