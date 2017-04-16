b216308dbc97fb6d63c32e3e015841f18a36f7f6
aniruddha-pratap <aniruddhapratap.singh@sjsu.edu>
2017-04-15 23:12:25 -0700

    yUML code

--- UMLJavaParser_202/src/UMLJavaParser/ConnectyUML.java
+++ UMLJavaParser_202/src/UMLJavaParser/ConnectyUML.java
@@ -1,28 +1,42 @@
 package UMLJavaParser;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-
+import java.util.*;
 public class ConnectyUML {
 
-	public static void connectToYUml(String passedCode) {
+	public static void connectToYUml(String passedCode, String imagePath) {
 
+		HttpURLConnection connection = null;
+		OutputStream outputStream = null;
+		
         try {
             String link = "http://yuml.me/diagram/plain/class/" + passedCode
                     + ".png";
             URL connectUrl = new URL(link);
-            HttpURLConnection connection = (HttpURLConnection) connectUrl.openConnection();
+            connection = (HttpURLConnection) connectUrl.openConnection();
             connection.setRequestMethod("GET");
             connection.setRequestProperty("Accept", "application/json");
-
             if (connection.getResponseCode() != 200) {
                 throw new RuntimeException(
                         "Failed : HTTP error code : " + connection.getResponseCode());
             }
-            connection.disconnect();
+            outputStream = new FileOutputStream(new File(imagePath));
+            byte[] readBytes = new byte[1024];
+			while(connection.getInputStream().read(readBytes) != -1){
+				outputStream.write(readBytes);
+            }
+			outputStream.close();
         } catch (Exception e) {
             System.out.println(e);
-        } 
+        }
+        finally{
+        	connection.disconnect();
+        	
+        }
     }
 	
 }
